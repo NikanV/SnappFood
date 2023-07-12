35a2fa9772d8529019cd409089da136c13fba039
alirezakazemeini <a.kazemeini@nobitex.net>
2023-07-12 10:18:51 +0330

    refactorization started

--- Frontend/aliases.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '~icons': './components/icons',
-  '~general': './assets/styles/general',
-  '~sass_mixins': './assets/styles/mixins',
-  '~endpoints': './utilities/endpoints'
-}
